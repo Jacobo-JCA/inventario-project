14e231b58d5f6353100a2f70a5f7d034e270e343
Esteban0rtiz <esteban.ortizpinsag@gmail.com>
2023-05-15 09:58:07 -0500

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+
+import { ProductsComponent } from './components/products/products.component';
 import { NosotrosComponent } from './components/nosotros/nosotros.component';
 
 const routes: Routes = [
 
   {path: 'home', component: HomeComponent},
   {path: 'nosotros', component: NosotrosComponent},
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
+
+  {path: 'products', component: ProductsComponent},
+  {path: '', redirectTo : 'home', pathMatch: 'full'}
 
 ];
 
