84af99879cf47a47db3e65329a9e1c13160ce5c2
ferveral92 <fernando_1992veral@hotmail.com>
2023-05-15 09:31:32 -0500

    ValidacionRutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
 
 const routes: Routes = [
 
   {path: 'home', component: HomeComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'nosotros', component: NosotrosComponent}
 
 ];
 
