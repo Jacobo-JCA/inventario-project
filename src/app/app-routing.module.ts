c15ae539da738098bae76b78121ce7010ca17079
Jacob Capa <jacobowiz.7@protonmail.com>
2023-05-15 22:43:14 -0500

    deleteprecio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,13 @@ import { HomeComponent } from './components/home/home.component';
 import { NosotrosComponent } from './components/nosotros/nosotros.component';
 import { ContactosComponent } from './components/contactos/contactos.component';
 import { CombosComponent } from './components/combos/combos.component';
-import { PrecioComponent } from './components/precio/precio.component';
 
 const routes: Routes = [
 
   {path: 'home', component: HomeComponent},
   {path: 'nosotros', component: NosotrosComponent},
   {path: 'contactos', component: ContactosComponent},
-  {path: 'combos', component: CombosComponent},
-  {path: 'precio', component: PrecioComponent},
+  {path: 'combos', component: CombosComponent}
 
 
   {path: '', redirectTo: 'home', pathMatch: 'full'},
