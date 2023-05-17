e83f380a4669208fab4b9284721af2ab71028684
ferveral92 <fernando_1992veral@hotmail.com>
2023-05-17 07:52:55 -0500

    cambiorouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NosotrosComponent } from './components/nosotros/nosotros.component';
 import { ContactosComponent } from './components/contactos/contactos.component';
+import { VentasComponent } from './components/ventas/ventas.component';
+import { AdministrarProductosComponent } from './components/administrar-productos/administrar-productos.component';
 
 
 const routes: Routes = [
@@ -10,8 +12,8 @@ const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'nosotros', component: NosotrosComponent},
   {path: 'contactos', component: ContactosComponent},
- 
-
+  {path: 'ventas', component: VentasComponent},
+  {path: 'administrar', component: AdministrarProductosComponent},
 
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: '**', pathMatch: 'full', redirectTo: 'home'}
