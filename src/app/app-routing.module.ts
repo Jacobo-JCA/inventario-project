18f66d97ae2b1dd853ac29800ae0b7a41c6899ac
ferveral92 <fernando_1992veral@hotmail.com>
2023-05-17 07:33:52 -0500

    CambioNuevooo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,26 +4,14 @@ import { HomeComponent } from './components/home/home.component';
 
 import { NosotrosComponent } from './components/nosotros/nosotros.component';
 import { ContactosComponent } from './components/contactos/contactos.component';
-<<<<<<< HEAD
-import { CombosComponent } from './components/combos/combos.component';
-import { VentasComponent } from './components/ventas/ventas.component';
-import { AdministrarProductosComponent } from './components/administrar-productos/administrar-productos.component';
-=======
 import { PrecioComponent } from './components/precio/precio.component';
->>>>>>> Jvera
 
 const routes: Routes = [
 
   {path: 'home', component: HomeComponent},
   {path: 'nosotros', component: NosotrosComponent},
   {path: 'contactos', component: ContactosComponent},
-<<<<<<< HEAD
-  {path: 'combos', component: CombosComponent},
-  {path: 'ventas', component: VentasComponent},
-  {path: 'administrar', component: AdministrarProductosComponent},
-=======
   {path: 'precio', component: PrecioComponent},
->>>>>>> Jvera
 
 
   {path: '', redirectTo: 'home', pathMatch: 'full'},
